030c7949855c9e5fbb0c0ade0c485474c79ccf03
mrana007 <rana.es.bc@gmail.com>
2023-11-24 22:40:48 +0600

    implement banner section at home page

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import "react-awesome-slider/dist/styles.css";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+const Banner = () => {
+  return (
+    <div>
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false} // should stop playing on user interaction
+        interval={2000}>
+        <div data-src="https://i.ibb.co/Mg9NYyL/img1.jpg"/>
+        <div data-src="https://i.ibb.co/YhVh4SX/img2.jpg"/>
+        <div data-src="https://i.ibb.co/BfvXjWj/img3.jpg"/>
+        <div data-src="https://i.ibb.co/41MJKyp/img4.jpg"/>
+        <div data-src="https://i.ibb.co/zNSvWKs/img5.jpg"/>
+        <div data-src="https://i.ibb.co/g6vTmL3/img6.jpg"/>
+      </AutoplaySlider>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/apartment">Apartment</Link></li>
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navOptions}
+            </ul>
+          </div>
+          <div className="flex items-center">
+          <a className="btn btn-ghost text-xl">Quantum Tower</a>
+          <img className="w-10" src="https://i.ibb.co/wMjm0hx/house.png" alt="apartment" />
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        </div>
+        {/* login info */}
+        <div className="navbar-end md:pr-16">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://i.ibb.co/cxddwLx/placeholder.jpg" alt="" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-black bg-opacity-40 text-white rounded-box w-52">
+              <li>
+                <p className="justify-between">Profile</p>
+              </li>
+              <li>
+                <Link to="/dashboard">DashBoard</Link>
+              </li>
+              <li>
+                <button>Logout</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
