1384327394a3321700ab1b60de549b6c99e92b98
mrana007 <rana.es.bc@gmail.com>
2023-11-27 00:40:49 +0600

    implement errorpage

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import Home from "../pages/Home/Home/Home";
 import Apartment from "../pages/Apartment/Apartment";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage />,
     children: [
         {
             path: '/',
